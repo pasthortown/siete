65eda630f6644796b3b780a8586f29485a6eb7f3
pasthortown <pastorchessmaster@gmail.com>
2019-05-10 18:45:08 -0500

    ok

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   api_auth: 'http://localhost:8000/',
   api_dinardap: 'http://localhost:9000/',
   api_consultor: 'http://localhost:9001/',
+  api_exporter: 'http://localhost:9002/',
   api_base: 'http://localhost:8001/',
   api_alojamiento: 'http://localhost:8002/',
   api_financiero: 'http://localhost:8003/',
