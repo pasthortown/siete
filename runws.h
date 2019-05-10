b1c4cbc51f3e6e8c23801af92d091994fb71bfc2
pasthortown <pastorchessmaster@gmail.com>
2019-05-10 12:44:28 -0500

    ok

--- runws.h
+++ runws.h
@@ -1,6 +1,7 @@
 #!/bin/bash
 screen -AmdS dinardap php -S localhost:9000 -t server-dinardap/public
 screen -AmdS consultor php -S localhost:9001 -t server-consultor/public
+screen -AmdS exporter php -S localhost:9002 -t server-exporter/public
 screen -AmdS auth php -S localhost:8000 -t server-auth/public
 screen -AmdS base php -S localhost:8001 -t server-base/public
 screen -AmdS alojamiento php -S localhost:8002 -t server-alojamiento/public
