3731898e36cc11b88c8b935b0c3796ffe9b624a3
pasthortown <pastorchessmaster@gmail.com>
2019-05-10 14:48:03 -0500

    ok

--- server-exporter/app/Http/Controllers/Negocio/ExporterController.php
+++ server-exporter/app/Http/Controllers/Negocio/ExporterController.php
@@ -4,33 +4,14 @@ namespace App\Http\Controllers;
 
 use Validator;
 use Exception;
-use SoapClient;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
-use App\Exports\DataExport;
 
 class ExporterController extends Controller
 {
 
   function formulario_requisitos(Request $data) {
-    return Excel::download(new DataExport,"prueba.xlsx");
-  }
-
-  protected function httpGet($url, $data=NULL, $headers = NULL, $token) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    if(!empty($data)){
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    }
-    $headersSend = array();
-    array_push($headersSend, 'api_token:'.$token);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headersSend);
-    $response = curl_exec($ch);
-    if (curl_error($ch)) {
-        trigger_error('Curl Error:' . curl_error($ch));
-    }
-    curl_close($ch);
-    return $response;
+    $toReturn = [[1,2,3],[4,5,6]];
+    return json_encode($toReturn);
   }
 }
