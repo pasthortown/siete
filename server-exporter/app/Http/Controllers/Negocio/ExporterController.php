028f6a99a48593d7ebb80c9c71b57e5c19ee91e7
pasthortown <pastorchessmaster@gmail.com>
2019-05-10 14:58:45 -0500

    ok

--- server-exporter/app/Http/Controllers/Negocio/ExporterController.php
+++ server-exporter/app/Http/Controllers/Negocio/ExporterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\DataExporter;
 use Validator;
 use Exception;
 use Illuminate\Http\Request;
@@ -11,7 +12,10 @@ class ExporterController extends Controller
 {
 
   function formulario_requisitos(Request $data) {
-    $toReturn = [[1,2,3],[4,5,6]];
-    return json_encode($toReturn);
+    $export = new DataExporter([
+      [$data['id'], 2, 3],
+      [4, 5, 6]
+    ]);
+    return Excel::download($export, 'invoices.xlsx');
   }
 }
