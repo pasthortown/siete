2413ff528f550c7c8bc9b58ec3d63512ddbd78bb
pasthortown <pastorchessmaster@gmail.com>
2019-05-23 11:06:17 -0500

    ok

--- soaptestnuevo.php
+++ soaptestnuevo.php
@@ -5,28 +5,25 @@ class SoapTest
   protected function soap() {
     $opts = array(
         'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
+          'local_cert' => './backup/certificados/dinardapgobec.pem',
       )
     );
     $context = stream_context_create($opts);
     $wsdl = "https://interoperabilidad.dinardap.gob.ec/interoperador-v2?wsdl";
     $this->client = new SoapClient($wsdl, array(
-      'stream_context' => $context, 'trace' => true,
+      'context' => $context, 'trace' => true,
       'login' => 'InToMiNtUR', 'password' => 'H0K;c[viIHEogh')
     );  
     return $this->client;
   }
-    
+     
   public function Cedula($identificacion, $codigoPaquete) {
     $this->client = $this->soap();
     try
     {
       $result = $this->client->consultar(["identificacion"=>$identificacion, "codigoPaquete"=>$codigoPaquete]);
-      return json_encode($result,200);
     }catch (Exception $e) {
-      return $e;
+      return json_encode($e);
     }
   }
 }
