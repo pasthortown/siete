fc1c276c28ead797e67f67d0b188d6b23fb1d23f
pasthortown <pastorchessmaster@gmail.com>
2019-05-31 17:36:40 -0500

    ok

--- client/src/app/layout/EXTERNO/tarifario_rack/tarifario-rack.component.ts
+++ client/src/app/layout/EXTERNO/tarifario_rack/tarifario-rack.component.ts
@@ -1,3 +1,6 @@
+import { BedTypeService } from 'src/app/services/CRUD/ALOJAMIENTO/bedtype.service';
+import { BedType } from './../../../models/ALOJAMIENTO/BedType';
+import { CapacityTypeService } from './../../../services/CRUD/ALOJAMIENTO/capacitytype.service';
 import { StateService } from 'src/app/services/CRUD/ALOJAMIENTO/state.service';
 import { State } from 'src/app/models/ALOJAMIENTO/State';
 import { CapacityType } from './../../../models/ALOJAMIENTO/CapacityType';
@@ -8,6 +11,10 @@ import { User } from 'src/app/models/profile/User';
 import { UserService } from 'src/app/services/profile/user.service';
 import { RegisterType } from 'src/app/models/ALOJAMIENTO/RegisterType';
 import { RegisterTypeService } from 'src/app/services/CRUD/ALOJAMIENTO/registertype.service';
+import { Tariff } from 'src/app/models/ALOJAMIENTO/Tariff';
+import { TariffTypeService } from 'src/app/services/CRUD/ALOJAMIENTO/tarifftype.service';
+import { TariffType } from 'src/app/models/ALOJAMIENTO/TariffType';
+import { Capacity } from 'src/app/models/ALOJAMIENTO/Capacity';
 
 @Component({
   selector: 'app-tarifario-rack',
@@ -19,6 +26,7 @@ export class TarifarioRackComponent implements OnInit {
   registers_mintur = [];
   registerMinturSelected: Register = new Register();
   user: User = new User();
+  mostrarDatos = false;
   config: any = {
     paging: true,
     filtering: {filterString: ''},
@@ -34,15 +42,25 @@ export class TarifarioRackComponent implements OnInit {
   estado_tramite_selected_code: String = '1';
   register_types: RegisterType[] = [];
   specific_states: State[];
+  tarifarioResponse: Tariff[] = [];
+  tarifarioRack = {cabecera: [], valores: []};
+  tarifas: any[] = [];
+  categories_registers: RegisterType[] = [];
+  categorySelectedCode = '-';
+  alowed_bed_types: BedType[] = []; 
 
   constructor(private registerDataService: RegisterService,
               private userDataService: UserService,
               private register_typeDataService: RegisterTypeService,
               private stateDataService: StateService,
+              private capacityTypeDataService: CapacityTypeService,
+              private bedTypeDataService: BedTypeService,
+              private tariffTypeDataService: TariffTypeService,
     ) {}
 
   ngOnInit() {
     this.getUser();
+    this.getTariffs();
     this.getStates();
   }
 
@@ -235,8 +253,21 @@ export class TarifarioRackComponent implements OnInit {
    }
 
    selectRegister(register: Register) {
-    this.registerDataService.get_tarifario(register.id).then( r => {
-      console.log(r);
+    this.registerDataService.get_register_data(register.id).then( r => {
+      this.registerMinturSelected = r.register;
+      this.registerMinturSelected.status = r.status.state_id;
+      this.registerMinturSelected.capacities_on_register = r.capacities_on_register as Capacity[];
+      this.getTarifarioRack(register.id);
+      this.getAllowedInfo();
+      this.alowed_capacity_types = [];
+      this.capacityTypeDataService.get_filtered_by_register_type(this.registerMinturSelected.register_type_id).then( r2 => {
+        this.alowed_capacity_types = r2 as CapacityType[];
+        this.registerMinturSelected.capacities_on_register.forEach(capacity => {
+          this.getMaxBed(capacity);
+          this.calcBeds(capacity);
+        });
+        this.calcSpaces();
+      }).catch( e => { console.log(e); });
     }).catch( e => { console.log(e); });
    }
 
@@ -255,6 +286,184 @@ export class TarifarioRackComponent implements OnInit {
        }
     });
     return toReturn;
+   }
+ 
+   getTariffs() {
+    this.tarifas = [];
+    this.tarifarioRack = {cabecera: [{valor:'Tipo de Habitación', padre: '', hijo: ''}], valores: []};
+    this.tariffTypeDataService.get().then( r => {
+       const result = r as TariffType[];
+       result.forEach(father => {
+          if(father.father_code == '-'){
+             const tariff_father: TariffType = father;
+             const tariff_child: TariffType[] = [];
+             result.forEach(child => {
+                if(child.father_code == father.code) {
+                   child.is_reference = father.is_reference;
+                   tariff_child.push(child);
+                   this.tarifarioRack.cabecera.push({valor:'Tarifa por ' + tariff_father.name + ' en ' + child.name, padre:tariff_father.name, hijo: child.name});
+                }
+             });
+             this.tarifas.push({father: tariff_father, childs: tariff_child});
+          }
+       });
+    }).catch( e => { console.log(e); });
+   }
+
+   getTarifarioRack(register_id: number) {
+    this.registerDataService.get_tarifario(register_id).then( r => {
+       this.tarifarioResponse = r as Tariff[];
+       let max_year = 0;
+       this.tarifarioResponse.forEach(element => {
+          if(element.year > max_year){
+             max_year = element.year;
+          }
+       });
+       this.tarifarioRack.valores.forEach(element => {
+          element.tariffs.forEach(tariffRack => {
+             const tariff = tariffRack.tariff;
+             this.tarifarioResponse.forEach(tariffResponse => {
+                if(tariffResponse.tariff_type_id == tariff.tariff_type_id && tariffResponse.year == max_year && tariffResponse.capacity_type_id == tariff.capacity_type_id) {
+                   tariffRack.tariff.price = tariffResponse.price;
+                }
+             });
+          });
+       });
+    }).catch( e => { console.log(e); });
+   }
+
+   getAllowedInfo() {
+    this.getBedTypes();
+    this.getCapacityTypes();
+   }
+
+   getCapacityTypes() {
+    this.alowed_capacity_types = [];
+    this.capacityTypeDataService.get_filtered_by_register_type(this.registerMinturSelected.register_type_id).then( r => {
+       this.alowed_capacity_types = r as CapacityType[];
+    }).catch( e => { console.log(e); });
+   }
+
+   getBedTypes() {
+    this.alowed_bed_types = [];
+    this.bedTypeDataService.get_filtered_by_register_type(this.registerMinturSelected.register_type_id).then( r => {
+       this.alowed_bed_types = r as BedType[];
+    }).catch( e => console.log(e) );
+   }
+
+   getMaxBed(capacity: Capacity) {
+    this.alowed_capacity_types.forEach(capacityType => {
+       if(capacityType.id == capacity.capacity_type_id) {
+          capacity.max_bed = capacityType.bed_quantity;
+          capacity.max_spaces = capacityType.spaces;
+          if(capacityType.spaces == 999) {
+             capacity.max_bed = 0;
+             capacity.max_spaces = 0;
+             capacity.editable = true;
+          } else {
+             capacity.editable = false;
+             capacity.editable_beds = capacityType.editable_beds;
+             capacity.editable_spaces = capacityType.editable_spaces;
+          }
+       }
+    });
+   }
+
+   calcBeds(capacity: Capacity) {
+    capacity.total_spaces = 0;
+    let beds_declared = 0;
+    capacity.beds_on_capacity.forEach(bed => {
+       let places = 0;
+       beds_declared += bed.quantity;
+       this.alowed_bed_types.forEach(bedType => {
+          if(bedType.id == bed.bed_type_id) {
+             places = bed.quantity;
+          }
+       });
+       capacity.total_spaces += places;
+    });
+    if(beds_declared == capacity.max_bed){
+       capacity.max_bed_ok = true;
+    }else {
+       capacity.max_bed_ok = false;
+    }
+    this.validateTariffs();
+   }
+
+   validateTariffs() {
+    return true;
+   }
+
+   calcSpaces(capacity?) {
+    if(typeof capacity !== 'undefined') {
+       this.alowed_capacity_types.forEach(capacityType => {
+          if (capacityType.id == capacity.capacity_type_id) {
+             if (capacityType.editable_spaces) {
+                capacity.max_spaces = 0;
+             } else {
+                capacity.max_spaces = capacityType.spaces * capacity.quantity;
+             }
+             if (capacity.max_bed > capacityType.bed_quantity){
+                capacity.max_bed = capacityType.bed_quantity;
+             }
+             if (capacity.max_bed == 0){
+                capacity.max_bed = 1;
+             }
+          }
+       });
+    }
+    this.registerMinturSelected.total_spaces = 0;
+    this.registerMinturSelected.total_habitations = 0;
+    this.registerMinturSelected.total_beds = 0;
+    if (this.tarifarioRack.valores.length == this.registerMinturSelected.capacities_on_register.length) {
+       for (let i = 0; i<this.registerMinturSelected.capacities_on_register.length ; i++) {
+          this.tarifarioRack.valores[i].idTipoCapacidad = this.registerMinturSelected.capacities_on_register[i].capacity_type_id;
+       }
+    } else {
+       this.tarifarioRack.valores = [];
+       this.registerMinturSelected.capacities_on_register.forEach(capacity => {
+          const childs = [];
+          let idTipoCapacidad = capacity.capacity_type_id;
+          this.tarifas.forEach(tariffType => {
+             tariffType.childs.forEach(tariffTypeChild => {
+                const es_referencia = tariffType.father.is_reference;
+                let plazasHabitacion = 0;
+                this.alowed_capacity_types.forEach(capacityType => {
+                   if (capacityType.id == idTipoCapacidad) {
+                      plazasHabitacion = capacityType.spaces;
+                   }
+                });
+                let nombreDivision = '';
+                nombreDivision = tariffTypeChild.name;
+                const tariff = new Tariff();
+                tariff.tariff_type_id = tariffTypeChild.id;
+                tariff.price = 0;
+                tariff.capacity_type_id = capacity.capacity_type_id;
+                const today = new Date();
+                tariff.year = today.getFullYear();
+                let newChild = {nombreDivision: nombreDivision, tariff: tariff, isReference: es_referencia, plazasHabitacion: plazasHabitacion};
+                childs.push(newChild);
+             });
+          });
+          let topush = {idTipoCapacidad: idTipoCapacidad, tariffs: childs};
+          this.tarifarioRack.valores.push(topush);
+       });
+    }
+    this.registerMinturSelected.capacities_on_register.forEach(capacity => {
+       capacity.editable = false;
+       this.alowed_capacity_types.forEach(capacityType => {
+          if (capacityType.id == capacity.capacity_type_id) {
+             capacity.editable_beds = capacityType.editable_beds;
+             capacity.editable_spaces = capacityType.editable_spaces;
+             if (capacityType.spaces == 999) {
+                capacity.editable = true;
+             }
+          }
+       });
+       this.registerMinturSelected.total_spaces += capacity.max_spaces;
+       this.registerMinturSelected.total_habitations += capacity.quantity;
+       this.registerMinturSelected.total_beds += (capacity.max_bed * capacity.quantity);
+    });
    }
  
 }
--- client/src/app/layout/EXTERNO/tarifario_rack/tarifario-rack.module.ts
+++ client/src/app/layout/EXTERNO/tarifario_rack/tarifario-rack.module.ts
@@ -1,3 +1,4 @@
+import { BedTypeService } from './../../../services/CRUD/ALOJAMIENTO/bedtype.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Ng2TableModule } from 'ng2-table/ng2-table';
 import { FormsModule } from '@angular/forms';
@@ -9,7 +10,8 @@ import { TarifarioRackComponent } from './tarifario-rack.component';
 import { UserService } from 'src/app/services/profile/user.service';
 import { StateService } from 'src/app/services/CRUD/ALOJAMIENTO/state.service';
 import { RegisterTypeService } from 'src/app/services/CRUD/ALOJAMIENTO/registertype.service';
-
+import { TariffTypeService } from 'src/app/services/CRUD/ALOJAMIENTO/tarifftype.service';
+import { CapacityTypeService } from './../../../services/CRUD/ALOJAMIENTO/capacitytype.service';
 @NgModule({
   imports: [CommonModule,
     TarifarioRackRoutingModule,
@@ -17,6 +19,12 @@ import { RegisterTypeService } from 'src/app/services/CRUD/ALOJAMIENTO/registert
     NgbModule,
     Ng2TableModule],
   declarations: [TarifarioRackComponent],
-  providers: [RegisterService, UserService, StateService, RegisterTypeService]
+  providers: [RegisterService,
+    UserService, 
+    StateService, 
+    RegisterTypeService,
+    BedTypeService, 
+    TariffTypeService,
+    CapacityTypeService]
 })
 export class TarifarioRackModule {}
