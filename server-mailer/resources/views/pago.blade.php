a3c9d38d3c47878693edec522ac5538f8701beaf
pasthortown <pastorchessmaster@gmail.com>
2019-06-24 12:10:12 -0500

    fiananciero a externo

--- server-mailer/resources/views/pago.blade.php
+++ server-mailer/resources/views/pago.blade.php
@@ -7,33 +7,33 @@
     </div>
 </div>
 <p>Estimado/a {{ $information['para'] }}:</p>
-<p>Ha recibido una solicitud de {{ $information['tramite'] }} que ha sido asignada por Coordinador Zonal, por favor revise su bandeja en la plataforma SITURIN.</p>
-<h3>INFORMACIÓN DEL ESTABLECIMIENTO</h3>
+<p>Por favor cancele en cualquier agencia o cajero inteligente de Banco Pacífico a nivel nacional, el valor correspondiente a su contribución Uno por Mil.</p>
+<h3>DESCRIPCIÓN DE PAGO</h3>
 <table style="border-collapse: collapse;width: 100%;border: 1px solid #ddd;">
     <tbody>
         <tr style="background-color: #f2f2f2;">
-            <th>RUC:</th><td>{{ $information['ruc'] }}</td><th>Provincia:</th><td>{{ $information['provincia'] }}</td>
+            <th>Contribución Uno por Mil:</th><td>{{ $information['amount_to_pay_base'] }}</td>
         </tr>
         <tr>
-            <th>Nombre Comercial:</th><td>{{ $information['nombreComercial'] }}</td><th>Cantón:</th><td>{{ $information['canton'] }}</td>
+            <th>Multas:</th><td>{{ $information['amount_to_pay_fines'] }}</td>
         </tr>
         <tr style="background-color: #f2f2f2;">
-            <th>Fecha Solicitud:</th><td>{{ $information['fechaSolicitud'] }}</td><th>Parroquia:</th><td>{{ $information['parroquia'] }}</td>
+            <th>Intereses: </th><td>{{ $information['amount_to_pay_taxes'] }}</td>
         </tr>
         <tr>
-            <th>Actividad:</th><td>{{ $information['actividad'] }}</td><th>Calle Principal:</th><td>{{ $information['callePrincipal'] }}</td>
-        </tr>
-        <tr style="background-color: #f2f2f2;">
-            <th>Clasificación:</th><td>{{ $information['clasificacion'] }}</td><th>Calle Intersección:</th><td>{{ $information['calleInterseccion'] }}</td>
-        </tr>
-        <tr>
-            <th>Categoría:</th><td>{{ $information['categoria'] }}</td><th>Numeración:</th><td>{{ $information['numeracion'] }}</td>
-        </tr>
-        <tr style="background-color: #f2f2f2;">
-            <th>Tipo de Solicitud:</th><td>{{ $information['tipoSolicitud'] }}</td><th></th><td></td>
+            <th>Total a pagar:</th><td>{{ $information['amount_to_pay'] }}</td>
         </tr>
     </tbody>
 </table>
+<p><strong>El pago deberá hacerlo a partir del siguiente día laborable de la fecha que reciba esta notificación a su correo electrónico</strong>, únicamente debe acercarse a las ventanillas del Banco de Pacífico, dicte el número de RUC e  indique que realizará un pago al Ministerio de Turismo.</p>
+<p>Si usted es usuario de Banco Pacífico y desea realizar el pago a través del sistema <strong>Intermático</strong>, los pasos son: </p>
+<ol>
+<li> Transferencias, pagos y recargas</li>
+<li>Instituciones,</li>
+<li>Seleccione Empresas,</li>
+<li>MIN TURISMO; y </li>
+<li>En el campo código: digite el número de RUC. </li>
+</ol>
 <div style="width:100%; float:left;" >
     <div style="width:25%; float:left;">&nbsp;</div>
     <div style="width:50%; float:left;">
