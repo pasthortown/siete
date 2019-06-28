8659e3a475542b78945153a04e1cdc5d3c7dd223
pasthortown <pastorchessmaster@gmail.com>
2019-06-28 16:35:40 -0500

    ok

--- server-dinardap-nuevo/app/Http/Controllers/Negocio/DinardapController.php
+++ server-dinardap-nuevo/app/Http/Controllers/Negocio/DinardapController.php
@@ -4,98 +4,80 @@ namespace App\Http\Controllers;
 
 use Validator;
 use Exception;
-use SoapClient;
 use Illuminate\Http\Request;
 
 class DinardapController extends Controller
 {
-  protected function soap() {
-    $opts = array(
-        'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-      )
-    );
+  protected function buildParameters($parameters) {
+		$toReturn = "         <parametros>\n";
+		foreach ($parameters as $parameter) {
+			$toReturn .= "            <parametro>\n";
+			$toReturn .= "               <nombre>".$parameter["nombre"]."</nombre>\n";
+			$toReturn .= "               <valor>".$parameter["valor"]."</valor>\n";
+			$toReturn .= "            </parametro>\n";
+		}
+		$toReturn .= "         </parametros>\n";
+		return $toReturn;
+	}
+
+	protected function connect($parameters) {
+		$parameters_xml = $this->buildParameters($parameters);
+		$soapUrl = "https://interoperabilidad.dinardap.gob.ec/interoperador-v2?wsdl";
+		$soapUser = "InToMiNtUR";
+		$soapPassword = "H0K;c[viIHEogh";
+		$xml_post_string = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:int=\"http://interoperabilidad.dinardap.gob.ec/interoperador/\">\n";
+		$xml_post_string .= "   <soapenv:Header/>\n";
+		$xml_post_string .= "   <soapenv:Body>\n";
+		$xml_post_string .= "      <int:consultar>\n" . $parameters_xml;
+		$xml_post_string .= "      </int:consultar>\n";
+		$xml_post_string .= "   </soapenv:Body>\n";
+		$xml_post_string .= "</soapenv:Envelope>\n";
+		
+		$headers = array(
+			"Content-type: text/xml;charset=\"utf-8\"",
+			"Accept: text/xml",
+			"Cache-Control: no-cache",
+			"Pragma: no-cache",
+			"SOAPAction: http://interoperabilidad.dinardap.gob.ec/interoperador/consultar", 
+			"Content-length: ".strlen($xml_post_string),
+		);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_URL, $soapUrl);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_USERPWD, $soapUser.":".$soapPassword); 
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_post_string);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$curl_response = curl_exec($ch);
+		curl_close($ch);
+		if(!$curl_response) {
+			return response()->json("No se obtuvo respuesta",400);
+		} else{
+      try {
+        $consultarResponse = explode('<ns2:consultarResponse xmlns:ns2="http://interoperabilidad.dinardap.gob.ec/interoperador/">', $curl_response)[1];
+        $response  = explode('</ns2:consultarResponse>', $consultarResponse)[0];
+        return response()->json(simplexml_load_string($response),200);
+      } catch (Exception $e) {
+        return response()->json($e->getMessage(),400);
+      }
+		}
+  }
   
-    $context = stream_context_create($opts);
-    $wsdl = "https://interoperabilidad.dinardap.gob.ec/interoperador-v2?wsdl";
-  
-    try
-    {
-      $this->client = new SoapClient($wsdl, array(
-        'stream_context' => $context, 'trace' => true,
-        'login' => 'InToMiNtUR', 'password' => 'H0K;c[viIHEogh')
-      );  
-      return $this->client;
-    }catch ( Exception $e) {
-      return $e;
-    }
-  }
-    
-  public function checkSoap(Request $request) {
-    $url = "https://interoperabilidad.dinardap.gob.ec/interoperador-v2?wsdl";
-    $options["connection_timeout"] = 25;
-    $options["location"] = $url;
-    $options["trace"] = 1;
-    $options["login"] = 'InToMiNtUR';
-    $options["password"] = 'H0K;c[viIHEogh';
-    $opts = array(
-        'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-      )
-    );
-    $context = stream_context_create($opts);
-    
-    $client = new SoapClient($url,$options);
-
-    $identificacion = '1720364049';
-    $codigoPaquete = 2112;
-    $result = $client->consultar(["identificacion"=>$identificacion, "codigoPaquete"=>$codigoPaquete]);
-    return response()->json($result,200);
-  }
-
-  public function Cedula(Request $request) {
-    $data = $request->json()->all();
-    $identificacion = $data['identificacion'];
-    $codigoPaquete = 2112;
-    $this->client = $this->soap();
-    try
-    {
-      $result = $this->client->consultar(["identificacion"=>$identificacion, "codigoPaquete"=>$codigoPaquete]);
-      return response()->json($result,200);
-    }catch (Exception $e) {
-      return $e;
-    }
-  }
-
   public function RUC(Request $request) {
-    $data = $request->json()->all();
-    $identificacion = $data['numeroIdentificacion'];
-    $codigoPaquete = 50;
-    $this->client = $this->soap();
-    try
-    {
-      $result = $this->client->getFichaGeneral(["numeroIdentificacion"=>$identificacion, "codigoPaquete"=>$codigoPaquete]);
-      return response()->json($result,200);
-    }catch (Exception $e) {
-      return $e;
-    }
+		$data = $request->json()->all();
+    $codigoPaquete = ["nombre"=>"codigoPaquete", "valor"=>"2115"];
+    $identificacion = ["nombre"=>"identificacion", "valor"=>$data["RUC"]];
+    $parameters = array($codigoPaquete, $identificacion);
+    return $this->connect($parameters);
   }
 
-  public function SuperCIAS(Request $request) {
-    $data = $request->json()->all();
-    $identificacion = $data['numeroIdentificacion'];
-    $codigoPaquete = 573;
-    $this->client = $this->soap();
-    try
-    {
-      $result = $this->client->getFichaGeneral(["numeroIdentificacion"=>$identificacion, "codigoPaquete"=>$codigoPaquete]);
-      return response()->json($result,200);
-    }catch (Exception $e) {
-      return $e;
-    }
+  public function cedula(Request $request) {
+		$data = $request->json()->all();
+    $codigoPaquete = ["nombre"=>"codigoPaquete", "valor"=>"2112"];
+    $identificacion = ["nombre"=>"identificacion", "valor"=>$data["identificacion"]];
+    $parameters = array($codigoPaquete, $identificacion);
+    return $this->connect($parameters);
   }
 }
