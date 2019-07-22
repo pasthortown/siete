a0f7a3737bfaf096ec63eaabb9487621e1dd55f7
pasthortown <pastorchessmaster@gmail.com>
2019-07-22 16:48:43 -0500

    ok

--- runws.h
+++ runws.h
@@ -8,5 +8,4 @@ screen -AmdS auth php -S localhost:8000 -t server-auth/public
 screen -AmdS base php -S localhost:8001 -t server-base/public
 screen -AmdS alojamiento php -S localhost:8002 -t server-alojamiento/public
 screen -AmdS financiero php -S localhost:8003 -t server-financiero/public
-screen -AmdS communications python3 communications.py
 
