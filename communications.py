e519d8cc1f47efe26d6acb70a1839d15502e5ff1
pasthortown <pastorchessmaster@gmail.com>
2019-07-22 15:38:45 -0500

    ok

---
+++ communications.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handle_message(message):
+    send(message, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host = '0.0.0.0', port=5005)
