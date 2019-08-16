5ef1dcc078368df29f55bd91d23ed95187dfa0f2
pasthortown <pastorchessmaster@gmail.com>
2019-08-16 09:41:30 -0500

    ok automatic

--- pusher.php
+++
@@ -1,5 +0,0 @@
-<?php
-$return = shell_exec('git add .');
-$return = shell_exec('git commit -m "ok automatic"');
-$salida = shell_exec('git push https://pasthortown:1509Charles*@github.com/pasthortown/siete --all');
-echo $salida;
