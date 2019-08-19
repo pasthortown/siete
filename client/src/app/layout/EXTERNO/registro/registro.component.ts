e8be36075fe0eae74d1e2c125002f985e97cd7e0
pasthortown <pastorchessmaster@gmail.com>
2019-08-19 14:40:04 -0500

    ok automatic

--- client/src/app/layout/EXTERNO/registro/registro.component.ts
+++ client/src/app/layout/EXTERNO/registro/registro.component.ts
@@ -1111,7 +1111,7 @@ export class RegistroComponent implements OnInit {
    this.declarationItemsToShow.forEach(group => {
       group.Category.total = 0;
       group.items.forEach(item => {
-         group.Category.total += item.valueItem.value * item.declaration_item.factor;
+         group.Category.total += item.valueItem.value * item.declarationItem.factor;
       });
    });
   }
