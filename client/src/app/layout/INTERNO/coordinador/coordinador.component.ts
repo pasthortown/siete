89856ca82284007fb860763a5b29b8ac1cbf2976
pasthortown <pastorchessmaster@gmail.com>
2019-08-19 21:35:47 -0500

    ok automatic

--- client/src/app/layout/INTERNO/coordinador/coordinador.component.ts
+++ client/src/app/layout/INTERNO/coordinador/coordinador.component.ts
@@ -2148,6 +2148,7 @@ export class CoordinadorComponent implements OnInit {
                this.registerApprovalFinanciero.id_user = 0;
                this.financialSelectedId = 0;
             }
+            console.log(this.registerApprovalFinanciero);
             this.checkIfIsAssignedFinanciero();
             this.checkAttachmentsFinanciero();
          }
