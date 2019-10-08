386419caeec4d6db7f7ca763538815161a85a7db
Azus-Alvarez <azuza83@hotmail.com>
2019-10-08 09:46:56 -0500

    admin rc

--- client/src/app/layout/INTERNO/administrators/admin-registro/admin-registro.component.ts
+++ client/src/app/layout/INTERNO/administrators/admin-registro/admin-registro.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrManager } from 'ng6-toastr-notifications';
+import { saveAs } from 'file-saver/FileSaver';
+import { AccountRolAssigment } from 'src/app/models/AUTH/AccountRolAssigment';
+import { AccountRol } from 'src/app/models/AUTH/AccountRol';
+import { User } from 'src/app/models/profile/User';
+import { AccountRolService } from 'src/app/services/CRUD/AUTH/accountrol.service';
 
 @Component({
   selector: 'app-admin-registro',
